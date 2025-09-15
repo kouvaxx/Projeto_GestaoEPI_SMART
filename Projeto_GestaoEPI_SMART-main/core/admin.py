c6467d6b6f1897e8a255165f45f2e1fcbb8e0d84
Jaisson Bertolini <59883181+kouvaxx@users.noreply.github.com>
2025-09-15 11:47:05 -0300

    Delete Projeto_GestaoEPI_SMART-main directory

--- Projeto_GestaoEPI_SMART-main/core/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Colaborador, Equipamento, Emprestimo
-
-admin.site.register(Colaborador)
-admin.site.register(Equipamento)
-admin.site.register(Emprestimo)
